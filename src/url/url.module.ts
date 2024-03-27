ae2bf04ae7eeb467576886b84a598206ab82ac44
KanekarSnehal <snehalkanekar43@gmail.com>
2024-03-28 01:18:24 +0530

    feat: add service in url module provider

--- src/url/url.module.ts
+++ src/url/url.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { UrlController } from './url.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Url } from './url.enttity';
+import { UrlService } from './url.service';
 
 @Module({
   controllers: [UrlController],
-  imports: [TypeOrmModule.forFeature([Url])]
+  imports: [TypeOrmModule.forFeature([Url])],
+  providers: [UrlService]
 })
 export class UrlModule {}
