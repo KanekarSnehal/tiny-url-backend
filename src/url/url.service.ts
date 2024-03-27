08f30538c16d21911781942bc805c9f9d346454f
KanekarSnehal <snehalkanekar43@gmail.com>
2024-03-28 01:18:39 +0530

    feat: add url service

---
+++ src/url/url.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Url } from './url.enttity';
+import { Repository } from 'typeorm';
+import { CreateUrlDto } from './url.schema';
+
+@Injectable()
+export class UrlService {
+    constructor(
+        @InjectRepository(Url)
+        private urlRepository: Repository<Url>
+    ) { }
+
+    updateUrl(updateUrlData) {
+        return this.urlRepository.save(updateUrlData);
+    }
+}
