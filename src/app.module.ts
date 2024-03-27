e4f66a917d1eff12727031dcf3ab9ddab34247d9
KanekarSnehal <snehalkanekar43@gmail.com>
2024-03-27 21:49:24 +0530

    feat: add typeorm and other imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UrlModule } from './url/url.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('MYSQL_HOST'),
+        port: configService.get('MYSQL_PORT'),
+        username: configService.get('MYSQL_USER'),
+        password: configService.get('MYSQL_PASS'),
+        database: configService.get('MYSQL_DB'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService]
+    }),
+    UrlModule,
+    UserModule
+  ],
+  controllers: [],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
