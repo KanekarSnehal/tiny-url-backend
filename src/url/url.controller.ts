79d1ae31493387ecda7cb2186a1dd1cdebafd83e
KanekarSnehal <snehalkanekar43@gmail.com>
2024-03-28 01:17:33 +0530

    feat: add create url controller

--- src/url/url.controller.ts
+++ src/url/url.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes } from '@nestjs/common';
+import { ZodValidationPipePipe } from 'src/zod-validation-pipe/zod-validation-pipe.pipe';
+import { CreateUrlDto, createUrlSchema } from './url.schema';
+import { UrlService } from './url.service';
 
 @Controller('url')
-export class UrlController {}
+export class UrlController {
+    constructor(private urlService: UrlService) { }
+
+    @Post()
+    @UsePipes(new ZodValidationPipePipe(createUrlSchema))
+    createTinyUrl(@Body() createUrlDto: CreateUrlDto) {
+        const { long_url, custom_back_half, generate_qr, title } = createUrlDto;
+
+        this.urlService.updateUrl({...createUrlDto, id: 1, created_by: 1 });
+    }
+}
