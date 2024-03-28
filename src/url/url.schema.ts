7f54b9199a282ed6ade3b3fe41bc85e298abb43b
KanekarSnehal <snehalkanekar43@gmail.com>
2024-03-28 17:35:34 +0530

    fix: validation for long_url

--- src/url/url.schema.ts
+++ src/url/url.schema.ts
@@ -1,11 +1,13 @@
 import { z } from 'zod';
 
-export const createUrlSchema = z
+export const createUrlZObject = z
   .object({
-    long_url: z.string().url({ message: 'Invalid Url'}).min(1),
+    long_url: z.string().url({ message: 'Invalid Url'}),
     title: z.string(),
     custom_back_half: z.string(),
     generate_qr: z.boolean()
   }).partial();
+
+export const createUrlSchema = createUrlZObject.required({ long_url: true });
 
 export type CreateUrlDto = z.infer<typeof createUrlSchema>;
