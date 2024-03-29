fc7e7dead3f4694cd50c7b664f902fc753e835a6
KanekarSnehal <snehalkanekar43@gmail.com>
2024-03-29 13:36:00 +0530

    feat+fix: add get list of tiny url, add try ctach for create tiny url

--- src/url/url.controller.ts
+++ src/url/url.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Ip, Post, UsePipes } from '@nestjs/common';
-import { ZodValidationPipePipe } from 'src/zod-validation-pipe/zod-validation-pipe.pipe';
+import { Body, Controller, Get, Ip, Post, UsePipes } from '@nestjs/common';
+import { ZodValidationPipe } from 'src/zod-validation-pipe/zod-validation-pipe.pipe';
 import { CreateUrlDto, createUrlPayloadSchema } from './url.schema';
 import { UrlService } from './url.service';
 import { generateUniqueHash } from '../utils/generateUniqueHash';
@@ -10,30 +10,43 @@ const QRCode = require('qrcode');
 export class UrlController {
     constructor(private urlService: UrlService, private configService: ConfigService) { }
 
-    @Post()
-    @UsePipes(new ZodValidationPipePipe(createUrlPayloadSchema))
-    async createTinyUrl(@Body() createUrlDto: CreateUrlDto, @Ip() ipAddress: string) {
-        const { generate_qr } = createUrlDto;
-        const userId = 1;
-        let qrCode = null;
-
-        // generate hash
-        const hash = generateUniqueHash(ipAddress);
-
-        // generate short url
-        const shortUrl = `${this.configService.get('BACKEND_URL')}/${hash}`
-
-        // generate qr if generate_qr is true
-        if(generate_qr) {
-            try {
-                qrCode = await QRCode.toDataURL(shortUrl, {  type: 'image/jpeg' });
-              } catch (err) {
-                console.error(err)
-                throw err;
-              }
+    @Get()
+    async getListOfTinyUrlByUserId() {
+        try {
+            const userId = 1;
+            return this.urlService.getListOfTinyUrlByUserId(userId);
+        } catch(error) {
+            throw error;
         }
+    }
 
-        // store in db
-        this.urlService.crreateTinyUrl({ ...createUrlDto, id: hash, created_by: userId, qr_code: qrCode });
+    @Post()
+    @UsePipes(new ZodValidationPipe(createUrlPayloadSchema))
+    async createTinyUrl(@Body() createUrlDto: CreateUrlDto, @Ip() ipAddress: string) {
+        try {
+            const { generate_qr } = createUrlDto;
+            const userId = 1;
+            let qrCode = null;
+    
+            // generate hash
+            const hash = generateUniqueHash(ipAddress);
+    
+            // generate short url
+            const shortUrl = `${this.configService.get('BACKEND_URL')}/${hash}`
+    
+            // generate qr if generate_qr is true
+            if(generate_qr) {
+                qrCode = await QRCode.toDataURL(shortUrl, {  type: 'image/jpeg' });
+            }
+    
+            // store in db
+            this.urlService.createTinyUrl({ ...createUrlDto, id: hash, created_by: userId, qr_code: qrCode });
+            return {
+                status: 'success',
+                message: 'Tiny url created successfully'
+            };
+        } catch(error) {
+            throw error;
+        }
     }
 }
