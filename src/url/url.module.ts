153573688e56d65a3640009c35ad9ee6f4aef6b0
KanekarSnehal <snehalkanekar43@gmail.com>
2024-03-29 13:01:01 +0530

    fix: add config service in providers

--- src/url/url.module.ts
+++ src/url/url.module.ts
@@ -3,10 +3,11 @@ import { UrlController } from './url.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Url } from './url.enttity';
 import { UrlService } from './url.service';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [UrlController],
   imports: [TypeOrmModule.forFeature([Url])],
-  providers: [UrlService]
+  providers: [UrlService, ConfigService]
 })
 export class UrlModule {}
