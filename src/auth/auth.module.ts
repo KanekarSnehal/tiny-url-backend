7c8181dee858157a7697bb88ea88ca1d0023fac9
KanekarSnehal <snehalkanekar43@gmail.com>
2024-04-04 17:09:06 +0530

    chor: import service in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/user.entity';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, UserService]
+  providers: [AuthService, UserService, JwtService, ConfigService],
+  imports: [TypeOrmModule.forFeature([User])],
+
 })
 export class AuthModule {}
