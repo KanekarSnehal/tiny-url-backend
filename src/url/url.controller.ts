972438b3d30c2e0d946fb31e71da12ff77a9ce51
KanekarSnehal <snehalkanekar43@gmail.com>
2024-04-05 17:33:14 +0530

    fix:+feat add param check, add express response, add redirection to long url

--- src/url/url.controller.ts
+++ src/url/url.controller.ts
@@ -1,10 +1,12 @@
-import { Body, Controller, Delete, Get, Ip, Param, Post, Put, Query, UsePipes } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Ip, Param, Post, Put, Res, UsePipes } from '@nestjs/common';
 import { ZodValidationPipe } from 'src/zod-validation-pipe/zod-validation-pipe.pipe';
 import { CreateUrlDto, UpdateUrlDto, createUrlPayloadSchema, updateUrlPayloadSchema } from './url.schema';
 import { UrlService } from './url.service';
 import { generateUniqueHash } from '../utils/generateUniqueHash';
 import { ConfigService } from '@nestjs/config';
+import { Url } from './url.enttity';
 const QRCode = require('qrcode');
+import { Response } from 'express';
 
 @Controller('url')
 export class UrlController {
@@ -19,16 +21,22 @@ export class UrlController {
                 status: 'success',
                 data: response
             }
-        } catch(error) {
+        } catch (error) {
             throw error;
         }
     }
-    
-    @Get()
-    async getDetailsOfTinyUrlByUrlId(@Query('id') id: string) {
+
+    @Get(':id')
+    async getDetailsOfTinyUrlByUrlId(@Param() params: { id: string }, @Res() res: Response) {
         try {
-            return this.urlService.getDetailsOfTinyUrlByUrlId(id);
-        } catch(error) {
+            const urlId = params.id;
+            const response: Partial<Url> = await this.urlService.getDetailsOfTinyUrlByUrlId(urlId);
+            if (response && response.long_url) {
+                return res.redirect(HttpStatus.MOVED_PERMANENTLY, response.long_url);
+            }
+            res.status(HttpStatus.NOT_FOUND).sendFile('urlNotFound.html', { root: './src/static' });
+
+        } catch (error) {
             throw error;
         }
     }
@@ -40,25 +48,25 @@ export class UrlController {
             const { generate_qr } = createUrlDto;
             const userId = 1;
             let qrCode = null;
-    
+
             // generate hash
             const hash = generateUniqueHash(ipAddress);
-    
+
             // generate short url
             const shortUrl = `${this.configService.get('BACKEND_URL')}/${hash}`
-    
+
             // generate qr if generate_qr is true
-            if(generate_qr) {
-                qrCode = await QRCode.toDataURL(shortUrl, {  type: 'image/jpeg' });
+            if (generate_qr) {
+                qrCode = await QRCode.toDataURL(shortUrl, { type: 'image/jpeg' });
             }
-    
+
             // store in db
             this.urlService.createTinyUrl({ ...createUrlDto, id: hash, created_by: userId, qr_code: qrCode });
             return {
                 status: 'success',
                 message: 'Tiny url created successfully'
             };
-        } catch(error) {
+        } catch (error) {
             throw error;
         }
     }
@@ -73,7 +81,7 @@ export class UrlController {
                 status: 'success',
                 message: 'Tiny url details updated successfully'
             };
-        } catch(error) {
+        } catch (error) {
             throw error;
         }
     }
@@ -86,7 +94,7 @@ export class UrlController {
                 status: 'success',
                 message: 'Tiny url deleted successfully'
             };
-        } catch(error) {
+        } catch (error) {
             throw error;
         }
     }
