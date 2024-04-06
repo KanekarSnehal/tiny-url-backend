737f0d96c7a5da144b8f14f8ca9d1c4316944a8a
KanekarSnehal <snehalkanekar43@gmail.com>
2024-04-07 02:06:22 +0530

    feat: add qr code service

---
+++ src/qr-code/qr-code.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { QrCode } from './qr-code.enitity';
+import { CreateQrCodeServiceDto, UpdateQrCodeServiceDto } from './qr-code.schema';
+
+@Injectable()
+export class QrCodeService {
+    constructor(
+        @InjectRepository(QrCode)
+        private qrCodeRepository: Repository<QrCode>
+    ) { }
+
+    getListOfQrCodeByUserId(userId: number) {
+        return this.qrCodeRepository.find({
+            where: {
+                created_by: userId
+            },
+            order: {
+                created_at: 'DESC'
+            }
+        });
+    }
+
+    createQrCode(createQrCodeData: CreateQrCodeServiceDto) {
+        return this.qrCodeRepository.save(createQrCodeData);
+    }
+
+    getQrCodeByUrlId(urlId: string) {
+        return this.qrCodeRepository.findOne({
+            where: {
+                url_id: urlId
+            }
+        });
+    }
+
+    updateQrCodeDetails(id: number, updateQrCodeData: UpdateQrCodeServiceDto) {
+        return this.qrCodeRepository.update(id, updateQrCodeData);
+    }
+}
