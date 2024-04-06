040b83b56bd50cc389efba54948ebc2d787e7c77
KanekarSnehal <snehalkanekar43@gmail.com>
2024-04-07 02:07:16 +0530

    fix: add qr code imports in url module

--- src/url/url.module.ts
+++ src/url/url.module.ts
@@ -4,10 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Url } from './url.enttity';
 import { UrlService } from './url.service';
 import { ConfigService } from '@nestjs/config';
+import { QrCodeService } from 'src/qr-code/qr-code.service';
+import { QrCode } from 'src/qr-code/qr-code.enitity';
 
 @Module({
   controllers: [UrlController],
-  imports: [TypeOrmModule.forFeature([Url])],
-  providers: [UrlService, ConfigService]
+  imports: [TypeOrmModule.forFeature([Url, QrCode])],
+  providers: [UrlService, ConfigService, QrCodeService]
 })
 export class UrlModule {}
