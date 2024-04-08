4e3792af4b65f74e74815b6f88255fe968186a2e
KanekarSnehal <snehalkanekar43@gmail.com>
2024-04-09 00:26:13 +0530

    feat: add analytics import in url module

--- src/url/url.module.ts
+++ src/url/url.module.ts
@@ -6,10 +6,12 @@ import { UrlService } from './url.service';
 import { ConfigService } from '@nestjs/config';
 import { QrCodeService } from 'src/qr-code/qr-code.service';
 import { QrCode } from 'src/qr-code/qr-code.enitity';
+import { AnalyticsService } from 'src/analytics/analytics.service';
+import { Analytics } from 'src/analytics/analytics.entity';
 
 @Module({
   controllers: [UrlController],
-  imports: [TypeOrmModule.forFeature([Url, QrCode])],
-  providers: [UrlService, ConfigService, QrCodeService]
+  imports: [TypeOrmModule.forFeature([Url, QrCode, Analytics])],
+  providers: [UrlService, ConfigService, QrCodeService, AnalyticsService]
 })
 export class UrlModule {}
