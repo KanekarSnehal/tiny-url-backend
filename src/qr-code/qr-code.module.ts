48c184e8351eb825e0ee3ea3e8ad3ba7891684af
KanekarSnehal <snehalkanekar43@gmail.com>
2024-04-09 15:52:00 +0530

    feat: add analytics service and entity import in qr module

--- src/qr-code/qr-code.module.ts
+++ src/qr-code/qr-code.module.ts
@@ -5,10 +5,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { QrCode } from './qr-code.enitity';
 import { Url } from 'src/url/url.enttity';
 import { UrlService } from 'src/url/url.service';
+import { AnalyticsService } from 'src/analytics/analytics.service';
+import { Analytics } from 'src/analytics/analytics.entity';
 
 @Module({
   controllers: [QrCodeController],
-  providers: [QrCodeService, UrlService],
-  imports: [TypeOrmModule.forFeature([QrCode, Url])]
+  providers: [QrCodeService, UrlService, AnalyticsService],
+  imports: [TypeOrmModule.forFeature([QrCode, Url, Analytics])]
 })
 export class QrCodeModule {}
