138fba3121f5142a570958fa8dac34b4b94fe754
KanekarSnehal <snehalkanekar43@gmail.com>
2024-04-13 15:13:27 +0530

    fix: refactor auth guard, add logout token check

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,42 +1,48 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { CanActivate, ExecutionContext, Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 import { Observable } from 'rxjs';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService, private configService: ConfigService, private reflector: Reflector) { }
+  constructor(private jwtService: JwtService, private configService: ConfigService, private reflector: Reflector, @Inject(CACHE_MANAGER) private cacheManager: Cache) { }
 
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const request = context.switchToHttp().getRequest();    
-    const allowUnauthorizedRequest = this.reflector.get<boolean>('allowUnauthorizedRequest', context.getHandler());
 
-    if(allowUnauthorizedRequest) return true;
+    return new Promise<boolean>(async (resolve) => {
 
-    const token = request.headers['authorization'];
+      const request = context.switchToHttp().getRequest();    
+      const allowUnauthorizedRequest = this.reflector.get<boolean>('allowUnauthorizedRequest', context.getHandler());
+  
+      if(allowUnauthorizedRequest) return resolve(true);
+  
+      const token = request.headers['authorization'];
+      
+      // check if token is present in request headers
+      if (!token) {
+        return resolve(false);
+      }
+      
+      // check if logout token is present in cache
+      const isLoggedOut = await this.cacheManager.get(`${token}`);
+      if(isLoggedOut) return resolve(false);
 
-    if (!token) {
-      return false;
-    }
+      // verify token
+      const decoded = await this.jwtService.verify(token.replace('Bearer', '').trim(),
+      {
+        secret: this.configService.get('JWT_SECRET_KEY')
+      });
+      
+      if(!decoded) return resolve(false);
 
-    // verify token
-    try {
-      const decoded = this.jwtService.verify(token.replace('Bearer', '').trim(),
-        {
-          secret: this.configService.get('JWT_SECRET_KEY')
-        });
 
-      if(!decoded) return false;
       request.user = decoded;
-
-    } catch (error) {
-      console.log("Error in auth guard", error)
-      return false;
-    }
-
-    return true;
+      return resolve(true);
+    });
   }
 }
