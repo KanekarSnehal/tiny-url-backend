bf257fe0a9f5d3936e6689665ba788202884e53e
KanekarSnehal <snehalkanekar43@gmail.com>
2024-04-28 22:48:47 +0530

    fix: add validation for login and signup schema

--- src/auth/auth.schema.ts
+++ src/auth/auth.schema.ts
@@ -3,9 +3,9 @@ import { z } from 'zod';
 // login schema
 export const loginZObject = z
   .object({
-    name: z.string(),
-    email: z.string(),
-    password: z.string(),
+    name: z.string().min(1),
+    email: z.string().email(),
+    password: z.string().min(1),
   }).partial();
 
 export const loginPayloadSchema = loginZObject.required({ email: true, password: true });
@@ -15,9 +15,9 @@ export type LoginDto = z.infer<typeof loginPayloadSchema>;
 // signup schema
 export const signupPayloadSchema = z
   .object({
-    name: z.string(),
-    email: z.string(),
-    password: z.string(),
+    name: z.string().min(1),
+    email: z.string().email(),
+    password: z.string().min(1),
   }).required();
 
 export type SignupDto = z.infer<typeof signupPayloadSchema>;
