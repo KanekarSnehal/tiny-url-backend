8fb716a3fd5793a5b05fe045b71b5154b0d0ef75
KanekarSnehal <snehalkanekar43@gmail.com>
2024-05-07 23:09:45 +0530

    fix: add try catch for auth guard

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -15,34 +15,36 @@ export class AuthGuard implements CanActivate {
   ): boolean | Promise<boolean> | Observable<boolean> {
 
     return new Promise<boolean>(async (resolve) => {
-
-      const request = context.switchToHttp().getRequest();    
-      const allowUnauthorizedRequest = this.reflector.get<boolean>('allowUnauthorizedRequest', context.getHandler());
+      try{
+        const request = context.switchToHttp().getRequest();    
+        const allowUnauthorizedRequest = this.reflector.get<boolean>('allowUnauthorizedRequest', context.getHandler());
+    
+        if(allowUnauthorizedRequest) return resolve(true);
+    
+        const token = request.headers['authorization'];
+        
+        // check if token is present in request headers
+        if (!token) {
+          return resolve(false);
+        }
+        
+        // check if logout token is present in cache
+        const isLoggedOut = await this.cacheManager.get(`${token}`);
+        if(isLoggedOut) return resolve(false);
   
-      if(allowUnauthorizedRequest) return resolve(true);
+        // verify token
+        const jwtToken = token.replace('Bearer', '').trim();
+        const decoded = await this.jwtService.verify(jwtToken, { secret: this.configService.get('JWT_SECRET_KEY') });
+        
+        if(!decoded) return resolve(false);
   
-      const token = request.headers['authorization'];
-      
-      // check if token is present in request headers
-      if (!token) {
+  
+        request.user = decoded;
+        return resolve(true);
+      } catch(error) {
         return resolve(false);
       }
-      
-      // check if logout token is present in cache
-      const isLoggedOut = await this.cacheManager.get(`${token}`);
-      if(isLoggedOut) return resolve(false);
 
-      // verify token
-      const decoded = await this.jwtService.verify(token.replace('Bearer', '').trim(),
-      {
-        secret: this.configService.get('JWT_SECRET_KEY')
-      });
-      
-      if(!decoded) return resolve(false);
-
-
-      request.user = decoded;
-      return resolve(true);
     });
   }
 }
